2389527a832888806dde4294effc0560bf98aa23
gkukucska <gkukucska91@gmail.com>
2025-08-10 12:47:34 +0200

    Added server side filtering, sorting and paging

--- CoreBlazor/Utils/DbContextExtensions.cs
+++ CoreBlazor/Utils/DbContextExtensions.cs
@@ -6,20 +6,13 @@ namespace CoreBlazor.Utils;
 
 public static class DbContextExtensions
 {
-    public static IEnumerable<INavigation> GetNavigations<TEntity>(this DbContext context) where TEntity : class
-    {
-        return context.Model.FindEntityType(typeof(TEntity))?.GetNavigations() ?? [];
-    }
-    public static bool IsNavigation<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class
-    {
-        return context.GetNavigations<TEntity>().Any(x=>x.Name == propertyInfo.Name);
-    }
+    public static IEnumerable<INavigation> GetNavigations<TEntity>(this DbContext context) where TEntity : class 
+        => context.Model.FindEntityType(typeof(TEntity))?.GetNavigations() ?? [];
 
-    public static IQueryable<TEntity> IncludeNavigations<TEntity>(this DbContext context)
-        where TEntity : class
-    {
-        var navigations = context.GetNavigations<TEntity>();
-        return navigations.Aggregate(context.Set<TEntity>().AsQueryable(),
-            (query, navigation) => query.Include(navigation.Name));
-    }
+    public static bool IsNavigation<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class 
+        => context.GetNavigations<TEntity>().Any(x => x.Name == propertyInfo.Name);
+
+    public static IQueryable<TEntity> DbSetWithDisplayableNavigations<TEntity>(this DbContext context) where TEntity : class 
+        => context.GetNavigations<TEntity>().Where(x=>x.PropertyInfo?.IsDisplayable() ?? false)
+        .Aggregate(context.Set<TEntity>().AsQueryable(),(query, navigation) => query.Include(navigation.Name));
 }
--- CoreBlazor/Utils/PropertyInfoExtensions.cs
+++ CoreBlazor/Utils/PropertyInfoExtensions.cs
@@ -5,20 +5,57 @@ namespace CoreBlazor.Utils;
 
 public static class PropertyInfoExtensions
 {
+
+    private static readonly MethodInfo _stringContainsMethod = typeof(string).GetMethod(nameof(string.Contains), BindingFlags.Instance | BindingFlags.Public, [typeof(string)])!;
+    private static readonly MethodInfo _stringStartsWithMethod = typeof(string).GetMethod(nameof(string.StartsWith), BindingFlags.Instance | BindingFlags.Public, [typeof(string)])!;
+    private static readonly MethodInfo _stringEndsWithMethod = typeof(string).GetMethod(nameof(string.EndsWith), BindingFlags.Instance | BindingFlags.Public, [typeof(string)])!;
+    private static readonly MethodInfo _stringEqualsMethod = typeof(string).GetMethod(nameof(string.Equals), BindingFlags.Instance | BindingFlags.Public, [typeof(string)])!;
+
+
     public static Expression<Func<T, TProperty>> GetMemberAccessExpressionWithConversion<T, TProperty>(this PropertyInfo pi)
     {
         var defaultExpression = Expression.Parameter(typeof(T));
-        Expression propertyOrField = Expression.Property(defaultExpression, pi);
+        Expression property = Expression.Property(defaultExpression, pi);
         if (pi.PropertyType != typeof(TProperty))
-            propertyOrField = Expression.Convert(propertyOrField, typeof(TProperty));
-        return Expression.Lambda<Func<T,TProperty>>(propertyOrField, defaultExpression);
+            property = Expression.Convert(property, typeof(TProperty));
+        return Expression.Lambda<Func<T,TProperty>>(property, defaultExpression);
     }
     public static Expression<Func<T, TProperty>> GetMemberAccessExpression<T, TProperty>(this PropertyInfo pi)
     {
         var defaultExpression = Expression.Parameter(typeof(T));
-        Expression propertyOrField = Expression.Property(defaultExpression, pi);
-        return Expression.Lambda<Func<T,TProperty>>(propertyOrField, defaultExpression);
+        Expression property = Expression.Property(defaultExpression, pi);
+        return Expression.Lambda<Func<T,TProperty>>(property, defaultExpression);
     }
 
     public static bool IsDisplayable(this PropertyInfo pi) => !pi.PropertyType.IsGenericType;
+
+    public static Expression<Func<TEntity, bool>> ContainsPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.FilterWithMethod<TEntity>(filter, _stringContainsMethod);
+    public static Expression<Func<TEntity, bool>> NotContainsPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.NegateFilterWithMethod<TEntity>(filter, _stringContainsMethod);
+    public static Expression<Func<TEntity, bool>> EqualsPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.FilterWithMethod<TEntity>(filter, _stringEqualsMethod);
+    public static Expression<Func<TEntity, bool>> NotEqualsPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.NegateFilterWithMethod<TEntity>(filter, _stringEqualsMethod);
+    public static Expression<Func<TEntity, bool>> StartsWithPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.FilterWithMethod<TEntity>(filter, _stringStartsWithMethod);
+    public static Expression<Func<TEntity, bool>> EndsWithPredicate<TEntity>(this PropertyInfo pi, string filter) where TEntity : class
+        => pi.FilterWithMethod<TEntity>(filter, _stringEndsWithMethod);
+
+    private static Expression<Func<TEntity, bool>> FilterWithMethod<TEntity>(this PropertyInfo pi, string filter, MethodInfo method) where TEntity : class
+    {
+        ParameterExpression entity = Expression.Parameter(typeof(TEntity));
+        MemberExpression member = Expression.Property(entity, pi);
+        var filterExpr = Expression.Call(member, method, Expression.Constant(filter, typeof(string)));
+        return Expression.Lambda<Func<TEntity, bool>>(filterExpr, entity);
+    }
+
+    private static Expression<Func<TEntity, bool>> NegateFilterWithMethod<TEntity>(this PropertyInfo pi, string filter, MethodInfo method) where TEntity : class
+    {
+        ParameterExpression entity = Expression.Parameter(typeof(TEntity));
+        MemberExpression member = Expression.Property(entity, pi);
+        Expression filterExpr = Expression.Call(member, method, Expression.Constant(filter, typeof(string)));
+        filterExpr = Expression.Not(filterExpr);
+        return Expression.Lambda<Func<TEntity, bool>>(filterExpr, entity);
+    }
 }
