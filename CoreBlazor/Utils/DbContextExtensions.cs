a09fd4099fd4b8a636ecda431c76e4369b0d128e
gkukucska <gkukucska91@gmail.com>
2025-08-21 08:58:28 +0200

    Removed editing option for PK if it is generated

--- CoreBlazor/Utils/DbContextExtensions.cs
+++ CoreBlazor/Utils/DbContextExtensions.cs
@@ -13,20 +13,46 @@ public static class DbContextExtensions
     public static bool IsNavigation<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class 
         => context.GetNavigations<TEntity>().Any(x => x.Name == propertyInfo.Name);
 
-    public static IQueryable<TEntity> DbSetWithDisplayableNavigations<TEntity>(this DbContext context) where TEntity : class 
-        => context.GetNavigations<TEntity>().Where(x=>x.PropertyInfo?.IsDisplayable() ?? false)
-        .Aggregate(context.Set<TEntity>().AsQueryable(),(query, navigation) => query.Include(navigation.Name));
-
     public static IProperty GetPrimaryKey<TEntity>(this DbContext context) where TEntity : class
     {
         var entityType = context.Model.FindEntityType(typeof(TEntity));
         if (entityType is null)
             throw new InvalidOperationException($"Entity type {typeof(TEntity).Name} not found in the model.");
         var primaryKey = entityType.FindPrimaryKey();
-        if (primaryKey is null or {Properties.Count: 0 })
+        if (primaryKey is null or { Properties.Count: 0 })
             throw new InvalidOperationException($"Primary key for entity type {typeof(TEntity).Name} not found.");
         return primaryKey.Properties[0];
     }
+
+    public static bool IsPrimaryKey<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class
+    {
+        var pk = context.GetPrimaryKey<TEntity>().PropertyInfo;
+        return pk.Name == propertyInfo.Name && pk.PropertyType == propertyInfo.PropertyType;
+    }
+
+    public static bool IsGeneratedPrimaryKey<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class
+    {
+        var pk = context.GetPrimaryKey<TEntity>();
+        return pk.PropertyInfo.Name == propertyInfo.Name && pk.PropertyInfo.PropertyType == propertyInfo.PropertyType && pk.ValueGenerated != ValueGenerated.Never;
+    }
+
+    public static IEnumerable<IForeignKey> GetForeignKeys<TEntity>(this DbContext context) where TEntity : class
+    {
+        var entityType = context.Model.FindEntityType(typeof(TEntity));
+        if (entityType is null)
+            throw new InvalidOperationException($"Entity type {typeof(TEntity).Name} not found in the model.");
+        return entityType.GetDeclaredForeignKeys();
+    }
+
+    public static bool IsForeignKey<TEntity>(this DbContext context, PropertyInfo propertyInfo) where TEntity : class
+    {
+        var foreignKeys = context.GetForeignKeys<TEntity>();
+        return foreignKeys.Select(fk=> fk.Properties[0].PropertyInfo).Any(fk=> fk.Name == propertyInfo.Name && fk.PropertyType == propertyInfo.PropertyType);
+    }
+
+    public static IQueryable<TEntity> DbSetWithDisplayableNavigations<TEntity>(this DbContext context) where TEntity : class
+        => context.GetNavigations<TEntity>().Where(x => x.PropertyInfo?.IsDisplayable() ?? false)
+        .Aggregate(context.Set<TEntity>().AsQueryable(), (query, navigation) => query.Include(navigation.Name));
 
     public async static Task<GridDataProviderResult<TEntity>> ApplyTo<TEntity>(this DbContext context, GridDataProviderRequest<TEntity> request) where TEntity : class
         => await context.DbSetWithDisplayableNavigations<TEntity>()
